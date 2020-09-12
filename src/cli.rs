3fe434f1a825255f865880234183e88643a65d3f
basu-dev <adbasudev54@gmail.com>
2020-09-12 21:58:50 +0545

    Problems I will work tomorrow

--- src/cli.rs
+++ src/cli.rs
@@ -3,16 +3,15 @@ pub fn run(){
     let number=206;
     println!("{}","Guessing Game" );
     println!("{}","Take a guess" );
-    let mut guess=String::new();
-    io::stdin().read_line(&mut guess).expect("There was some error");
-    fn guess_result(guess:u8){
+    let mut guess:i8;
+    io::stdin().read_line(guess).expect("There was some error");
+    fn guess_result(guess:i8){
         match guess {
             32=>println!("Guess Right{}", guess),
             _=>println!("Wrong guess {}",guess )
         };
     }
-    guess_result(&guess);
+    
+    guess_result(guess);
     println!("You guessed {}",guess );
-
-
 }
